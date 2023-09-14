4d71854b8f064213a6a0935e15990b12a2753926
haitham <haitham314s@gmail.com>
2023-09-14 23:10:36 +0400

    postgres connection success!

--- ecommerce/config.py
+++ ecommerce/config.py
@@ -5,7 +5,9 @@ APP_ENV = os.getenv("APP_ENV", "development")
 DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "admin")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "admin123")
 DATABASE_HOST = os.getenv("DATABASE_HOST", "postgres-service")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "ecommercedb")
+
 
 TEST_DATABASE_NAME = os.getenv("DATABASE_NAME", "test_ecommercedb")
 
